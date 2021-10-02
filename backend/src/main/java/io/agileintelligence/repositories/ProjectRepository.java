7935adb653b2b96f50eafc7f8bd0dd4d88dfd87e
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-10-01 22:44:24 -0300

    Project Object and Project Repository

---
+++ backend/src/main/java/io/agileintelligence/repositories/ProjectRepository.java
@@ -0,0 +1,11 @@
+package io.agileintelligence.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import io.agileintelligence.domain.Project;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+}
