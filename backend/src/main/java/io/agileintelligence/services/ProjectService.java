1f29eeee017708721ba067e6169e12eeb3867962
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-10-05 00:02:35 -0300

    Custom Exception for Unique Project Identifiers

--- backend/src/main/java/io/agileintelligence/services/ProjectService.java
+++ backend/src/main/java/io/agileintelligence/services/ProjectService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import io.agileintelligence.domain.Project;
+import io.agileintelligence.exceptions.ProjectIdException;
 import io.agileintelligence.repositories.ProjectRepository;
 
 @Service
@@ -13,8 +14,12 @@ public class ProjectService {
 	private ProjectRepository projectRepository;
 	
 	public Project saveOrUpdate(Project project) {
-		return projectRepository.save(project);
+		try {
+			project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+			
+			return projectRepository.save(project);
+		} catch (Exception e) {
+			throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists.");
+		}
 	}
-	
-	
 }
