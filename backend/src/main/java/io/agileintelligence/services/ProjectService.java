3d80717bac816939c6bea8bdfa428709a093f14b
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-10-06 00:22:52 -0300

    Delete an Existing Project

--- backend/src/main/java/io/agileintelligence/services/ProjectService.java
+++ backend/src/main/java/io/agileintelligence/services/ProjectService.java
@@ -38,4 +38,14 @@ public class ProjectService {
 	public List<Project> findAllProjects() {
 		return projectRepository.findAll();
 	}
+	
+	public void deleteProjectByIdentifier(String projectId) {
+		Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase()); 
+		
+		if(project == null) {
+			throw new ProjectIdException("Cannot Delete Project with ID '" + projectId + "'. This project does not exist.");
+		}
+		
+		projectRepository.delete(project);
+	}
 }
